d57cb59565a78a4358be0b6d3270b9be12a91748
mel0ndev <motherfker.jones1@gmail.com>
2024-02-13 08:53:28 -0500

    working build

--- app/utils/price.ts
+++ app/utils/price.ts
@@ -6,23 +6,23 @@ import { UniswapV2PoolAbi } from "@/app/hooks/abi/uniswapV2PoolAbi";
 
 const CLOCK = new Token(ChainId.SEPOLIA, "0x4eE24376Aa1CbC1Bb14462de3f15Ca62Ff9AF75F", 18); 
 
-export const useCreatePair = async (): Promise<Pair>  => {
-	const pairAddress = Pair.getAddress(CLOCK, WETH9[CLOCK.chainId]); 
-	
-	const {data: reserves} = useContractRead({
-		address: pairAddress as `0x${string}`,
-		abi: UniswapV2PoolAbi, 
-		functionName: "getReserves",
-		watch: true
-	}); 
-	
-	
-	const tokens = [CLOCK, WETH9[CLOCK.chainId]]; 
-	const [token0, token1] = tokens[0].sortsBefore(tokens[1]) ? tokens : [tokens[1], tokens[0]]; 
-
-	const pair = new Pair(CurrencyAmount.fromRawAmount(token0, reserves?.[0]), CurrencyAmount.fromRawAmount(token1, reserves?.[1])); 
-
-  return pair; 
+export const useCreatePair = async (): Promise<void>  => {
+//	const pairAddress = Pair.getAddress(CLOCK, WETH9[CLOCK.chainId]); 
+//	
+//	const {data: reserves} = useContractRead({
+//		address: pairAddress as `0x${string}`,
+//		abi: UniswapV2PoolAbi, 
+//		functionName: "getReserves",
+//		watch: true
+//	}); 
+//	
+//	
+//	const tokens = [CLOCK, WETH9[CLOCK.chainId]]; 
+//	const [token0, token1] = tokens[0].sortsBefore(tokens[1]) ? tokens : [tokens[1], tokens[0]]; 
+//
+//	const pair = new Pair(CurrencyAmount.fromRawAmount(token0, reserves?.[0]), CurrencyAmount.fromRawAmount(token1, reserves?.[1])); 
+//
+//  return pair; 
 }
 
 const useGetPrice = async () => {
